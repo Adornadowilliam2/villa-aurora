bc87a80004fcdfe4e7f65feb576ad24114cac507
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-12-01 20:38:59 +0800

    add changes

--- src/api/configuration.js
+++ src/api/configuration.js
@@ -1,1 +1,1 @@
-export const url = "http://localhost:8000/api";
+export const url = "https://backend-villa-aurora-production.up.railway.app/api";
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react({
-            include: "**/*.jsx",
-        }),
-    ],
+  plugins: [
+    react({
+      include: "**/*.jsx",
+    }),
+  ],
+  base: "/",
 });
